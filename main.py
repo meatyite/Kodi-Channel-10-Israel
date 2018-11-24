22dbf840c3d72931bde3838e259a5882deb8bcbe
sl4vkek <43294802+sl4vkek@users.noreply.github.com>
2018-11-24 14:58:21 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from urllib import urlencode
+from urlparse import parse_qsl
+import xbmcgui
+import xbmcplugin
+import xbmc
+
+xbmc.Player().play("http://nana10-hdl-il-sw.ctedgecdn.net/10tv_Desktop/index.m3u8?dvr")
